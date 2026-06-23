846ff15bee94f6081391aa09eea2f26810899c58
Syam Prasad <syamofficial1212@gmail.com>
2026-06-23 19:46:46 +0530

    Enhance SQL query with detailed comments
    Added comments explaining the SQL query steps and logic.

---
+++ Leetcode-SQL_50/JOINS/Replace_Employee_id_with_unique_identifier.sql
@@ -0,0 +1,116 @@
+/*Query*/
+
+Select eu.unique_id,e.name from Employees e LEFT JOIN EmployeeUNI eu ON e.id=eu.id;
+
+/*Step 1: Find Output
+
+Ask:
+
+Which columns do I need?
+Step 2: Find Common Key
+id
+employee_id
+customer_id
+product_id
+
+Use in:
+
+ON table1.id = table2.id
+Step 3: Choose JOIN
+
+Need all rows from first table?
+
+✅ YES → LEFT JOIN
+
+❌ NO → INNER JOIN
+
+Step 4: Missing Records Pattern
+LEFT JOIN ...
+WHERE right_table.id IS NULL
+
+Used for:
+
+Customers who never ordered
+Employees without managers
+Users without transactions
+Step 5: Self Join
+
+Same table joined with itself:
+
+Employee e
+JOIN Employee m
+
+Examples:
+
+Employee → Manager
+Student → Mentor*/
+
+
+/*Table: Employees
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| name          | varchar |
++---------------+---------+
+id is the primary key (column with unique values) for this table.
+Each row of this table contains the id and the name of an employee in a company.
+ 
+
+Table: EmployeeUNI
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| unique_id     | int     |
++---------------+---------+
+(id, unique_id) is the primary key (combination of columns with unique values) for this table.
+Each row of this table contains the id and the corresponding unique id of an employee in the company.
+ 
+
+Write a solution to show the unique ID of each user, If a user does not have a unique ID replace just show null.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Employees table:
++----+----------+
+| id | name     |
++----+----------+
+| 1  | Alice    |
+| 7  | Bob      |
+| 11 | Meir     |
+| 90 | Winston  |
+| 3  | Jonathan |
++----+----------+
+EmployeeUNI table:
++----+-----------+
+| id | unique_id |
++----+-----------+
+| 3  | 1         |
+| 11 | 2         |
+| 90 | 3         |
++----+-----------+
+Output: 
++-----------+----------+
+| unique_id | name     |
++-----------+----------+
+| null      | Alice    |
+| null      | Bob      |
+| 2         | Meir     |
+| 3         | Winston  |
+| 1         | Jonathan |
++-----------+----------+
+Explanation: 
+Alice and Bob do not have a unique ID, We will show null instead.
+The unique ID of Meir is 2.
+The unique ID of Winston is 3.
+The unique ID of Jonathan is 1.*/
